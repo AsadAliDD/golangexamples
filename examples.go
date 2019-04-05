3ee3fb8960a64a7808207df4b2b5c9fc5cca26b5
codejones <asadalisps@gmail.com>
2019-04-05 17:26:04 +0500

    All Funcstions

--- examples.go
+++ examples.go
@@ -1,6 +1,10 @@
 package golangexamples
 
-//Slice concataion
+import (
+	"github.com/ehteshamz/greetings"
+)
+
+//ConcatSlice Returns a Concatenanted byte array into string
 func ConcatSlice(sliceToConcat []byte) string {
 	var dummy string
 	for index := 0; index < len(sliceToConcat)-1; index++ {
@@ -10,8 +14,17 @@ func ConcatSlice(sliceToConcat []byte) string {
 	return dummy
 }
 
+//Encrypt
 func Encrypt(sliceToConcat []byte, ceaserCount int) string {
-
 	// var dummy strings
-	return "HELLO"
+	var dummy string
+	for index := 0; index < len(sliceToConcat); index++ {
+		dummy = dummy + string(sliceToConcat[index]+byte(ceaserCount))
+	}
+	return dummy
+}
+
+//EZGreetings
+func EZGreetings(dummy string) string {
+	return greetings.PrintGreetings(dummy)
 }
